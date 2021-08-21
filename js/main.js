4b48fde24af1ba560ae43ba3d704060148b20880
Vlad <fact1@ukr.net>
2021-08-21 20:50:46 +0300

    123

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(function() {
         prevArrow: ' <img class="slider__arrow slider__arrow-left" src="images/leftA.svg" alt="">',
         nextArrow: ' <img class="slider__arrow slider__arrow-right" src="images/rightA.svg" alt="">',
     });
+
+    $(".menu-btn").on("click", function() {
+        $(".menu__list").toggleClass("active");
+    });
 });
